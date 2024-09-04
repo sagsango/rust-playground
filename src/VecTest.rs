201ebeda3f5533c8d9cf9cf850eff42398c83f2c
SagarSingh <ssing214@uic.edu>
2024-09-03 20:03:51 -0500

    Vec iterator functions.

--- src/VecTest.rs
+++ src/VecTest.rs
@@ -27,4 +27,32 @@ pub fn test() {
 
     println!("Size of vector: {}", mem::size_of_val(&v));
     print!("Vector[0]: {}", v[0]);
+
+
+    /* XXX: Vector of given size */
+    let fruits = vec!["Apple", "Banana", "Cherry", "Date", "Fig"];
+    // convert into VecDeque
+    let fruits_VecDeque: std::collections::VecDeque<_> = fruits.into_iter().collect();
+    println!("Fruits: {:?}", fruits_VecDeque);
+    let val = match fruits_VecDeque.get(2) {
+        Some(val) => val,
+        None => &"Not found",
+    };
+    println!("Value: {}", val);
+
+    // vector of hights
+    let heights = vec![5.8, 6.0, 5.9, 5.10, 6.1];
+    let hight_sum: f64 = heights.iter().sum();
+
+    // more use of iter()
+    // We can use iter().x where x can be:
+    // sum, product, max, min, max_by, min_by, max_by_key, min_by_key, all, any, find, position, rposition, fold, reduce, collect, partition, partition_in_place, unzip, chain, cloned, copied, cycle, enumerate, filter, filter_map, flat_map, flatten, fuse, inspect, map, map_while, peekable, scan, skip, skip_while, step_by, take, take_while, zip
+
+
+    
+
+
+   
+    
+
 }
