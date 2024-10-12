e6b30b5c9174bd23cbfc3f60bf0431b6deef78ed
Sagar Singh <sagarsingh@Sagars-MacBook-Air.local>
2024-10-11 23:58:05 -0500

    More on memory managemnet

--- src/MemoryManagement/forget.rs
+++ src/MemoryManagement/forget.rs
@@ -12,7 +12,7 @@ forget(object) :
                 3. If object = Arc::clone(&parent_object), when parent_object goes out of scope, the refcount will be decremented by 1.
                 4. When refcount becomes 0 then only drop() will be called (object will be dropped).
                 
-                
+
 Initial Arc count: 1
 Initial Arc count: 2
 Initial Arc count: 2
@@ -181,9 +181,112 @@ fn test_forget_02() {
 }
 
 
+
+fn test_forget_03() {
+    // Define a custom struct that implements Drop
+    #[derive(Debug)]
+    struct YourStruct {
+        value: i32,
+    }
+
+    impl YourStruct {
+        fn new(value: i32) -> YourStruct {
+            YourStruct { value }
+        }
+    }
+
+    // Implement Drop for YourStruct to observe when it gets dropped
+    impl Drop for YourStruct {
+        fn drop(&mut self) {
+            println!(
+                "Dropping YourStruct with value: {} >>> DROPPED!!",
+                self.value
+            );
+        }
+    }
+
+    // Define an enum with two members
+    #[derive(Debug)]
+    enum MyEnum {
+        Int(i32),
+        Struct(YourStruct),
+    }
+
+    // Implement Drop for MyEnum to observe when any of its variants are dropped
+    impl Drop for MyEnum {
+        fn drop(&mut self) {
+            match self {
+                MyEnum::Int(val) => {
+                    println!("Dropping MyEnum::Int with value: {} >>> DROPPED!!", val);
+                }
+                MyEnum::Struct(s) => {
+                    println!("Dropping MyEnum::Struct with value: {:?} >>> DROPPED!!", s);
+                }
+            }
+        }
+    }
+
+
+    // Create a shared Arc object wrapping MyEnum (as Option) and set an initial value
+    let shared_data = Arc::new(Mutex::new(Some(MyEnum::Struct(YourStruct::new(42)))));
+
+    // Print the initial strong count
+    println!("Initial Arc count: {}", Arc::strong_count(&shared_data));
+
+    // Create a clone of the Arc
+    let data_clone = Arc::clone(&shared_data);
+    
+    println!("After cloning: Arc count: {}", Arc::strong_count(&shared_data));
+     
+    // Spawn a new thread to forget the Arc object after some time
+    let _ = thread::spawn(move || {
+        // Wait for 3 seconds before forgetting the Arc
+        thread::sleep(Duration::from_secs(3));
+        
+        // Print the Arc count before forgetting
+        println!("Before forgetting: Arc count: {}", Arc::strong_count(&data_clone));
+        
+        // Forget the Arc object (this will prevent its automatic dropping)
+        std::mem::forget(data_clone);
+        
+        println!("Arc object forgotten!");
+    }).join();
+    
+    println!("Parent continues: Arc count: {}", Arc::strong_count(&shared_data));
+
+    // Now, let's simulate making the shared data None after some time
+    let data_clone2 = Arc::clone(&shared_data);
+    
+    println!("After second clone: Arc count: {}", Arc::strong_count(&shared_data));
+    let _ = thread::spawn(move || {
+        // Wait for 5 seconds before setting the shared data to None
+        thread::sleep(Duration::from_secs(5));
+        
+        // Print the Arc count before setting to None
+        println!("Before setting to None: Arc count: {}", Arc::strong_count(&data_clone2));
+        
+        // Lock the mutex and set the data to None
+        let mut data = data_clone2.lock().unwrap();
+        *data = None; // Set to None
+        
+        println!("Shared data set to None!");
+        println!("Arc count after setting to None: {}", Arc::strong_count(&data_clone2));
+    }).join();
+
+    // Wait to see the output from threads
+    thread::sleep(Duration::from_secs(10));
+    
+    // Print final count before main exits
+    println!("Final Arc count: {}", Arc::strong_count(&shared_data));
+
+    // Print the value of the shared data
+    println!("Shared data: {:?}", shared_data.lock().unwrap());
+}
+
 pub fn test_forget() {
     test_forget_01();
     test_forget_02();
+    test_forget_03();
 }
 
 #[cfg(test)]
@@ -192,7 +295,7 @@ mod tests {
 
     #[test]
     fn run_test_forget() {
-        test_forget_02();
+        test_forget_03();
     }
 }
 
